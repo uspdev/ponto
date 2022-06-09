b7d949446c484bd8223676efb1d194ebb54310e4
Alessandro Costa de Oliveira <alecosta.web@gmail.com>
2022-06-09 12:52:16 -0300

    Config MonitorController

--- app/Http/Controllers/MonitorController.php
+++ app/Http/Controllers/MonitorController.php
@@ -3,8 +3,15 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Utils\ReplicadoTemp;
 
 class MonitorController extends Controller
 {
-    //
+    public function index()
+    {
+        $monitores = ReplicadoTemp::listarMonitores(config('ponto.codslamon'));
+        return view('monitores.index',[
+            'monitores' => $monitores
+        ]);
+    }
 }
