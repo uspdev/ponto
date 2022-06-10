bc08f1343db45abef0eaddbf60bef449486ee9f8
Alessandro Costa de Oliveira <alecosta.web@gmail.com>
2022-06-10 16:53:08 -0300

    Link para o registro

--- resources/views/monitores/show.blade.php
+++ resources/views/monitores/show.blade.php
@@ -32,15 +32,15 @@
       <div class="col-lg-7 p-3">
         <div class="card">
           <div class="card-body">
-            <h5 class="card-title">Ponto: {{ \Carbon\Carbon::today()->format('l, d \d\e F \d\e Y') }}</h5>
+            <h5 class="card-title">Registros: {{ \Carbon\Carbon::today()->format('l, d \d\e F \d\e Y') }}</h5>
             <ul class="list-group list-group-flush">
               @foreach ($registros as $registro)
                   <li class="list-group-item">
                     @if($registro->type == 'in')
-                        <i class="fas fa-sign-in-alt text-success"></i> {{ $registro->created_at->format('H:i') }} (Entrada)
+                      <i class="fas fa-sign-in-alt text-success"></i> <a href="/registros/{{ $registro->id }}">{{ $registro->created_at->format('H:i') }} (Entrada)</a>
                     @endif
                     @if($registro->type == 'out')
-                        <i class="fas fa-sign-out-alt text-danger"></i> {{ $registro->created_at->format('H:i') }} (Saída)
+                        <i class="fas fa-sign-out-alt text-danger"></i> <a href="/registros/{{ $registro->id }}">{{ $registro->created_at->format('H:i') }} (Saída)</a>
                     @endif
                   </li>
               @endforeach
