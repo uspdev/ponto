20247b7d5fdf30f25c1cf707c287b16ed790deff
Alessandro Costa de Oliveira <alecosta.web@gmail.com>
2023-12-04 14:17:39 -0300

    Lista somente lugares Pró-Aluno

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -5,13 +5,15 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 
 use App\Models\Place;
+use App\Models\Grupo;
 
 class IndexController extends Controller
 {
     public function index()
-    {
+    {   
         return view('index',[
-            'places' => Place::all(),
+            // Retorna somente os lugares com para monitores das salas Pró-Aluno ou Próaluno
+            'places' => Place::where('name', 'like', '%pro-aluno%')->orWhere('name', 'like', '%proaluno%')->get(),
         ]);
     }
 }
